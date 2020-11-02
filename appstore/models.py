40cf063ecd6571d86bc8368722d2cde80ba5f3ce
Marcelo <marcelo.servando@hotmai.com>
2020-11-02 21:57:09 +0100

    Changes to prepare the powershell script

--- appstore/models.py
+++ appstore/models.py
@@ -9,11 +9,13 @@ class Software(models.Model):
     version = models.CharField(max_length=20,blank=True,default='')
     publisher = models.CharField(max_length=50,blank=True)
     description = models.TextField(blank=True,default='')
-    detectionRules = models.JSONField(blank=True)
-    returnCodes = models.JSONField(blank=True)
+    detectionRules = models.JSONField(blank=True,default=dict)
+    returnCodes = models.JSONField(blank=True,default=dict )
     installStr = models.CharField(max_length=255,blank=True)
     uninstallStr = models.CharField(max_length=255,blank=True)
     enabled = models.BooleanField(default=True)
+    parameters = models.JSONField(blank=True,default=dict )
+    srcFile = models.CharField(max_length=255,default='')
 
 def __str__(self):
     return "software:{}".format(self.name)
